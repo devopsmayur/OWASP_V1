4a5f0bcee4e6822470123476420dcff4fd0f8ded
Mayur <99334268+devopsmayur@users.noreply.github.com>
2025-11-17 10:33:32 +0530

    Update ReactDOM import comment for version clarity

--- owasp/owasp-top10/frontend/src/index.js
+++ owasp/owasp-top10/frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // React 18+
+import ReactDOM from 'react-dom/client'; // React 189+
 import './App.css'; // Import global CSS
 import App from './App'; // Import main App component
 
